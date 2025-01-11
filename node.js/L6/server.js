4fa4303f145535b5cbd62727732ca17d4375d120
JobairIslam <jubairislamasif@gmail.com>
2025-01-11 12:09:36 +0600

    L6

--- node.js/L6/server.js
+++ node.js/L6/server.js
@@ -1,10 +1,11 @@
-//server creat
 const http = require("http");
 const port = 3000;
 const hostname = '127.0.0.1';
 
-const myServer = http.createServer((req, res) => { // Corrected 'rep' to 'req'
-    res.end("<h1>hello, I am your first server</h1>");
+const myServer = http.createServer((req, res) => { 
+    res.writeHead(202,{'Content-Type':'text/html'})
+    res.write("<h1>hello</h1>")
+    res.end()
 });
 
 myServer.listen(port, hostname, () => {
