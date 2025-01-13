f5d99119b517ade148184d294273dcfc0cf1f770
JobairIslam <jubairislamasif@gmail.com>
2025-01-13 10:55:37 +0600

    L9 server creating

--- node.js/L9/index.js
+++ node.js/L9/index.js
@@ -1,1 +1,10 @@
-console.log("jubair islam asif")
+const http=require('http');
+const { hostname } = require('os');
+const port=3000;
+const hostName='127.0.0.1';
+const server=http.createServer((req,res)=>{
+    res.end("wellcome")
+})
+server.listen(port,hostname,()=>{
+    console.log(`server is running at http://${hostName}:${port}`)
+})
