8ec8ff8c0531c049802a2805e009028f1fcffe0e
JobairIslam <jubairislamasif@gmail.com>
2025-01-18 22:22:01 +0600

    end method

--- express.js/L1/app.js
+++ express.js/L1/app.js
@@ -3,15 +3,19 @@ const app=express()
 //get request
 app.get("/",(req,res)=>{
     res.send("it a get request . it is HOME")
+    res.end()
 })
 app.post("/",(req,res)=>{
     res.send("it a post request . it is HOME")
+    res.end()
 })
 app.put("/",(req,res)=>{
     res.send("it a put request . it is HOME")
+    res.end()
 })
 app.delete("/",(req,res)=>{
     res.send("it a delete request . it is HOME")
+    res.end()
 })
 
 module.exports=app;
