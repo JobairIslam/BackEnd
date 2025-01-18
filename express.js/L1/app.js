92e24e5fde3ace7d3ff3ceb9dd5a00ce1a7892c0
JobairIslam <jubairislamasif@gmail.com>
2025-01-18 21:38:31 +0600

    get request

--- express.js/L1/app.js
+++ express.js/L1/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app=express()
 
+app.get("/",(req,res)=>{
+    res.send("it a get request . it is HOME")
+})
+
 module.exports=app;
--- express.js/L1/index.js
+++ express.js/L1/index.js
@@ -4,3 +4,4 @@ const port=3000;
 app.listen(port , ()=>{
     console.log(`server is running at http://localhost:${port}`)
 });
+ 
