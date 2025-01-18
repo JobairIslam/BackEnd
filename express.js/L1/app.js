6ab51867f2b692519e68113947a1d2890f912fa5
JobairIslam <jubairislamasif@gmail.com>
2025-01-18 21:49:38 +0600

    post request

--- express.js/L1/app.js
+++ express.js/L1/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app=express()
-
+//get request
 app.get("/",(req,res)=>{
     res.send("it a get request . it is HOME")
+})
+app.post("/",(req,res)=>{
+    res.send("it a post request . it is HOME")
+})
+app.put("/",(req,res)=>{
+    res.send("it a put request . it is HOME")
+})
+app.delete("/",(req,res)=>{
+    res.send("it a post request . it is HOME")
 })
 
 module.exports=app;
