45463b1d9c673a75dbbbe12c62edac03ac876faa
JobairIslam <jubairislamasif@gmail.com>
2025-01-19 20:54:04 +0600

    routing

--- express.js/L1/app.js
+++ express.js/L1/app.js
@@ -5,14 +5,26 @@ app.get("/",(req,res)=>{
     res.send("it a get request . it is HOME")
     res.end()
 })
+app.get("/register",(req,res)=>{
+    res.send("it a get request . it is Register")
+    res.end()
+})
+app.get("/login",(req,res)=>{
+    res.send("it a get request . it is LogIn")
+    res.end()
+})
+
+//post request
 app.post("/",(req,res)=>{
     res.send("it a post request . it is HOME")
     res.end()
 })
+//put request
 app.put("/",(req,res)=>{
     res.send("it a put request . it is HOME")
     res.end()
 })
+//delete request
 app.delete("/",(req,res)=>{
     res.send("it a delete request . it is HOME")
     res.end()
