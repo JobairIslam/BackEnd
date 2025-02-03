d019c7c210dd89b847001f2a8e5e4d054249d2a2
JobairIslam <jubairislamasif@gmail.com>
2025-02-03 10:00:21 +0600

    express L3

---
+++ express.js/L3/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port=3000;
+
+app.use("/",(req,res)=>{
+    res.send("jnrfu ernfjwerjnf freifj")
+})
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
