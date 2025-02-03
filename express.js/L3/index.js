6118ba75c8726bc3d35ba05feff9b2d55f62316b
JobairIslam <jubairislamasif@gmail.com>
2025-02-03 12:32:24 +0600

    l3

--- express.js/L3/index.js
+++ express.js/L3/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const port=3000;
+const bodyParser=require("body-parser")
 
-app.use("/",(req,res)=>{
-    res.send("jnrfu ernfjwerjnf freifj")
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+app.post("/user",(req,res)=>{
+    const name=req.body.name;
+    res.send(`welcome ${name}`)
 })
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
