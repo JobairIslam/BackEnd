40d5af5565aac97f195e9388274cb04984f864ad
JobairIslam <jubairislamasif@gmail.com>
2025-02-04 19:32:59 +0600

    calculator mini task

--- express.js/L5/index.js
+++ express.js/L5/index.js
@@ -15,8 +15,19 @@ app.get("/",(req,res)=>{
 app.get("/circle",(req,res)=>{
     res.sendFile(__dirname+"/circle.html")
 })
+app.post("/circle",(req,res)=>{
+    const radius = req.body.radius;
+    const area=Math.PI*radius*radius
+    res.send(`Area: ${area}`)
+})
 app.get("/tringle",(req,res)=>{
     res.sendFile(__dirname+"/tringle.html")
+})
+app.post("/tringle",(req,res)=>{
+    const base = req.body.base;
+    const height = req.body.height;
+    const area=0.5*base*height
+    res.send(`Area: ${area}`)
 })
 
 
