ab7abb18e20e02f7c323d320da93707a03cd9881
JobairIslam <jubairislamasif@gmail.com>
2025-02-06 14:34:47 +0600

    L9 end

---
+++ express.js/L9/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // Ensure the path to index.html is correct
+});
+
+// app.get('*', (req, res) => {
+//     res.send("404");
+// });
+//error handeling middleWare
+app.use((req,res,next)=>{
+    res.send("404 not found")
+})
+app.use((err,req,res,next)=>{
+    res.status(500).send("something went wrong")
+})
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
