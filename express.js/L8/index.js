5ee4ff450f9ac60fcafdf69d7a6b31e7aff358c8
JobairIslam <jubairislamasif@gmail.com>
2025-02-06 13:12:28 +0600

    L8 start

--- express.js/L8/index.js
+++ express.js/L8/index.js
@@ -2,6 +2,17 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT ||3000;
+
+
+//middleWare function
+const myMiddleWare=(req,res,next)=>{
+    console.log("Middle ware function")
+    next();
+}
+
+app.get("/",myMiddleWare,(req,res)=>{
+    res.send("home rout")
+})
 app.get('*', (req, res) => {
     res.send("404");
 });
