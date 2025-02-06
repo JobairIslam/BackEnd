cfa98144499cab4bcdc5ee1aed7cd3fce6eede23
JobairIslam <jubairislamasif@gmail.com>
2025-02-06 13:41:32 +0600

    L8 end

--- express.js/L8/index.js
+++ express.js/L8/index.js
@@ -7,13 +7,28 @@ const port = process.env.PORT ||3000;
 //middleWare function
 const myMiddleWare=(req,res,next)=>{
     console.log("Middle ware function")
-    next();
+
+    req.currTime=new Date(Date.now())
+    next(); 
 }
+
+//sob request er moddhe middleWare call na kore ai system use korle sob request er moddhe e middleWare ta thakbe
+
+// app.use(myMiddleWare)
+
 
 app.get("/",myMiddleWare,(req,res)=>{
     res.send("home rout")
+    console.log(req.currTime)
 })
-app.get('*', (req, res) => {
-    res.send("404");
-});
+// app.get('*', (req, res) => {
+//     res.send("404");
+// });
+//error handeling middleWare
+app.use((req,res,next)=>{
+    res.send("404 not found")
+})
+app.use((err,req,res,next)=>{
+    res.status(500).send("something went wrong")
+})
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
