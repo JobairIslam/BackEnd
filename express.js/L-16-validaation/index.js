a8ec0d2647b2a5b8e7d64a3e772788eb2a437591
JobairIslam <jubairislamasif@gmail.com>
2025-02-15 23:36:38 +0600

    starting

--- express.js/L-16-validaation/index.js
+++ express.js/L-16-validaation/index.js
@@ -11,21 +11,7 @@ app.get('/', (req, res) => {
     res.send('Home');
 });
 
-// Register route
-app.post('/api/register', (req, res) => {
-    console.log(req.body); // Log request body
-    try {
-        const { username, email, pass, dob } = req.body;
-        if (!username || !email) {
-            return res.status(400).json({ message: 'Username and email are required' });
-        }
-        return res.status(200).json({ newUser: { username, email, pass, dob } });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: error.message });
-    }
-});
-
+//name,email,pass,dob
 
 // Handle 404 errors (must be after all other routes)
 app.use((req, res) => {
