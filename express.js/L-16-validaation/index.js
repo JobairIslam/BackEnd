0304d95638ecbe62e0fff870ff14708ddfa86ca3
JobairIslam <jubairislamasif@gmail.com>
2025-02-17 21:30:48 +0600

    vali2

--- express.js/L-16-validaation/index.js
+++ express.js/L-16-validaation/index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-const {body}=require("express-validator")
+const {body, validationResult}=require("express-validator")
 
 app.get("/test", (req, res) => {
     res.send("test route");
@@ -14,7 +14,21 @@ app.get("/test", (req, res) => {
 // /api/register
 app.post("/api/register",
    //input validation
-   body("name").trim().notEmpty()
+   body("name")
+   .trim()
+   .notEmpty()
+   .withMessage("mane is required")
+   .isLength({min:5})
+   .withMessage("name must have at least 5 characters")
+   .isLength({max:15})
+   .withMessage("name can have max 15 characters")
+   ,(req,res,next)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array})
+    }
+    next()
+   }
     ,(req, res) => {
     try {
         const { name, email, pass, dob } = req.body;
