561683dd820fe0df49c1b4b4fd7d7343bcb408e1
JobairIslam <jubairislamasif@gmail.com>
2025-02-20 22:01:11 +0600

    joi validation2

--- express.js/L-17-joiVlidation/index.js
+++ express.js/L-17-joiVlidation/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const joi = require('joi');
 const app = express();
-const port = 3000;
+const port = 3008;
 
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 
@@ -10,3 +12,19 @@ app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 app.get("/test",(req,res)=>{
     res.json({message:"testing"})
 })
+
+//api/register
+
+app.post("/api/register", (req, res) => {
+    try {
+        console.log("Received data:", req.body);
+        const user={
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+        return res.send("user is created");
+    } catch (error) {
+        return res.send({ message: error.message });
+    }
+});
